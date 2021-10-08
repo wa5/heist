821cc96e8041a3ea2d38cbe362f68291bbd2f8f6
wa5 <waseemahmes116@gmail.com>
2021-10-08 09:14:25 +0530

    ytytyyuuy

---
+++ fullstack/backend/librararies/nodejs/moduleraper/script.js
@@ -0,0 +1,9 @@
+var address=" D:\all\all\ismuniv\heist\fullstack\backend\librararies\nodejs\moduleraper"
+
+((__dirname,__filename,requre,exports,defult)=>{
+    console.log("im hear----",__dirname)
+    console.log("im hear-file---",__filename)
+})()
+  
+  
+
